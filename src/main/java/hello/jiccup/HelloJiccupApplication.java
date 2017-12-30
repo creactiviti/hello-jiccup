49b6782f28b9bae4fcdf90a32b9f0ac859563f71
Cohen, Arik <arik.cohen@smithsonianchannel.com>
2017-12-30 13:39:46 -0500

    first commit

---
+++ src/main/java/hello/jiccup/HelloJiccupApplication.java
@@ -0,0 +1,12 @@
+package hello.jiccup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJiccupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJiccupApplication.class, args);
+	}
+}
